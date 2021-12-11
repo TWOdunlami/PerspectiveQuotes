be08eee0390babe3ababf12bb2f404b658ecad22
TWOdunlami <t_alvarez88@hotmail.com>
2021-12-11 06:10:38 -0500

    comment statement added for incomplete object.

--- quote.js
+++ quote.js
@@ -1,3 +1,4 @@
+// object below this line is incomplete.
 var quote = [
 "Every great dream begins with a dreamer. Always remember, you have within you the strength, the patience, and the passion to reach for the stars to change the world.",
 "No matter what accomplishments you make, somebody helped you.",
