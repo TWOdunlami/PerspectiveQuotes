8109b192797b0c2ad03202c0b37b07af0482c4e4
TWOdunlami <t_alvarez88@hotmail.com>
2021-12-20 17:28:43 -0500

    conde lines 2-5: string syntax updated to prevent errors.

--- quote.js
+++ quote.js
@@ -1,9 +1,8 @@
 // object below this line is incomplete.
-var quote = [
-"Every great dream begins with a dreamer. Always remember, you have within you the strength, the patience, and the passion to reach for the stars to change the world. - Harriet Thubman",
-"No matter what accomplishments you make, somebody helped you.",
-
-]
+function loveQuote() {
+    var quote = {
+        "Harriet Thubman" : '"Every great dream begins with a dreamer. Always remember, you have within you the strength, the patience, and the passion to reach for the stars to change the world."',}
+}
 /*
 function genQuote() {
 var randomNumber = Math.floor(Math.random() * quote.length);
